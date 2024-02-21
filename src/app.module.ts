b0d83b46add1fc69173f2cb938473cd80d98b7d2
jpernia <jpernia@captahydro.com>
2024-02-21 08:58:36 -0500

    change posgrests end docker local

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,15 +11,22 @@ import { PostsModule } from './posts/posts.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: parseInt(<string>process.env.MYSQL_PORT),
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_NAME,
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_NAME,
       entities: [],
       synchronize: true,
       autoLoadEntities: true,
+      ssl: process.env.POSTGRES_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === 'true'
+            ? { rejectUnauthorized: false }
+            : null,
+      },
     }),
     AuthModule,
     PostsModule,
